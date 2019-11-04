558c3b903b5e42b364cff86341813b647a26954d
alexlee-dev <alex@alexlee.dev>
2019-11-04 11:43:34 -0800

    Remove console logs

--- src/cli.js
+++ src/cli.js
@@ -12,16 +12,9 @@ const cli = async () => {
   const weatherData = await getWeatherData("584204204e65fad6a7709435");
 
   const surfWindow = {
-    // min: moment().format("MMMM Do YYYY, h:mm:ss: a"),
-    // max: moment(weatherData.sunlightTimes[0].sunset, "X").format(
-    //   "MMMM Do YYYY, h:mm:ss a"
-    // )
     min: moment().format("X"),
     max: weatherData.sunlightTimes[0].sunset
   };
-
-  // console.log(JSON.stringify(waveData, null, 2));
-  // console.log(waveData.length);
 
   const filteredWaveData = waveData.filter(
     waveHour =>
