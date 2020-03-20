eab1e664da776e032d9800e96dfe68c178f13d10
alexlee-dev <alex@alexlee.dev>
2020-03-20 12:47:48 -0700

    Add types to project root

---
+++ types.ts
@@ -0,0 +1,97 @@
+interface SunlightTime {
+  dawn: string;
+  dusk: string;
+  midnight: string;
+  sunrise: string;
+  sunset: string;
+}
+
+interface SurfData {
+  max: number;
+  min: number;
+  optimalScore: number;
+}
+
+export interface SurfSpotData {
+  continent: string;
+  country: string;
+  region: string;
+  name: string;
+  spotId: string;
+}
+
+export type SurfSpotGroup = SurfSpotData[];
+
+export interface SurfWindow {
+  max: string;
+  min: string;
+}
+
+interface SwellData {
+  direction: number;
+  directionMin: number;
+  height: number;
+  period: number;
+  optimalScore: number;
+}
+
+export type TimesData = string[];
+
+export interface WaveData {
+  surf: SurfData;
+  swells: SwellData[];
+  timestamp: string;
+}
+
+interface LocationData {
+  lat: number;
+  lon: number;
+}
+
+type UnitStandard = {
+  temperature: "F";
+  tideHeight: "FT";
+  waveHeight: "FT";
+  windSpeed: "KTS";
+};
+
+interface WaveResponseAssociated {
+  forecastLocation: LocationData;
+  location: LocationData;
+  offshoreLocation: LocationData;
+  units: UnitStandard;
+  utcOffset: number;
+}
+
+type WaveResponseData = {
+  wave: WaveDataGroup;
+};
+
+export interface WaveResponse {
+  associated: WaveResponseAssociated;
+  data?: WaveResponseData;
+}
+
+export interface WeatherData {
+  condition: string;
+  temperature: number;
+  timestamp: string;
+}
+
+export type WaveDataGroup = WaveData[];
+
+export interface WeatherDataGroup {
+  sunlightTimes: SunlightTime[];
+  weather: WeatherData[];
+}
+
+interface WeatherResponseAssociated {
+  units: UnitStandard;
+  utcOffset: number;
+  weatherIconPath: string;
+}
+
+export interface WeatherResponse {
+  associated: WeatherResponseAssociated;
+  data?: WeatherDataGroup;
+}
